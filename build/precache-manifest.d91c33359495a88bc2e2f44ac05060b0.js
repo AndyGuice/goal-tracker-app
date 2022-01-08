3bd95feb80964f54448fc0122524f04f71c75a90
Andy Guice <andy.guice@binderpos.com>
2022-01-08 15:04:29 -0600

    update procfile, add server.js for heroku hosting, create /build

---
+++ build/precache-manifest.d91c33359495a88bc2e2f44ac05060b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47e6283b91277a7202357b8d4bd83d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4703b6c43ba391068158",
+    "url": "/static/css/main.3d2b2a8d.chunk.css"
+  },
+  {
+    "revision": "33e3efd42a101ebd2246",
+    "url": "/static/js/2.1222a5fe.chunk.js"
+  },
+  {
+    "revision": "fde8a65b8d307872c8c44f6de8ee07de",
+    "url": "/static/js/2.1222a5fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4703b6c43ba391068158",
+    "url": "/static/js/main.ad105e42.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
