9423e5a7b72ab44891153f7b0876a0cd1a50c4b1
Andy Guice <andy.guice@binderpos.com>
2022-01-30 19:20:31 -0600

    add /build

---
+++ build/precache-manifest.5fc7b84a6400549a6a2862f0b14f053e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b67a5f5e21a2a6b3fe7bf5f91efa149",
+    "url": "/index.html"
+  },
+  {
+    "revision": "505afe8f1734aac2c9c7",
+    "url": "/static/css/main.37baa39d.chunk.css"
+  },
+  {
+    "revision": "52a002c2930064968b30",
+    "url": "/static/js/2.aa467107.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.aa467107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "505afe8f1734aac2c9c7",
+    "url": "/static/js/main.3909d8a2.chunk.js"
+  },
+  {
+    "revision": "6f5d15b6e63768feaab8",
+    "url": "/static/js/runtime-main.7c4d09e9.js"
+  }
+]);
