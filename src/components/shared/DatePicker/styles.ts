51e1c42aac063fb3c85f50061c7ee8f418f2c25e
Andy Guice <andy.guice@binderpos.com>
2022-01-30 19:44:37 -0600

    remove Shared to later re-add for github funkiness w/ netlify

--- src/components/shared/DatePicker/styles.ts
+++
@@ -1,9 +0,0 @@
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-
-export default makeStyles((theme: Theme) =>
-  createStyles({
-    button: {
-      padding: theme.spacing(2),
-    },
-  })
-);
