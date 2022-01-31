d5408c2b6a8f7ecd0b92dcd8f52759d912674a2f
Andy Guice <andy.guice@binderpos.com>
2022-01-30 19:54:50 -0600

    try fixing stupid github/netlify wonkiness

---
+++ src/components/Shared/ErrorDialog/ErrorDialog.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Typography from '@material-ui/core/Typography';
+
+const ErrorDialog = (props: any) => {
+  const { open, onClose, error } = props;
+
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+    >
+      <DialogTitle>Error</DialogTitle>
+      <Typography style={{ padding: 10 }}>
+        Error: {error}
+      </Typography>
+    </Dialog>
+  );
+};
+
+export default ErrorDialog;
