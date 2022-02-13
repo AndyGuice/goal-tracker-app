f58489cdc5d915b1c2117f7a21e58440a4e5c87c
Andy Guice <andy.guice@binderpos.com>
2022-02-13 15:08:29 -0600

    clean up consecutive streak function

--- src/helpers/tasks.ts
+++ src/helpers/tasks.ts
@@ -1,52 +1,36 @@
 import { addDays } from 'date-fns'
 
+function countConsecutiveCompletionDays(listOfDays: any) {
+  let count = 0
+
+  for (let i = 0; i < listOfDays.length; i++) {
+    const date = new Date(listOfDays[i])
+    const comparisonDate = new Date(listOfDays[i + 1])
+
+    if (addDays(date, -1).toLocaleDateString() !== comparisonDate.toLocaleDateString()) {
+      count++
+      break
+    } else {
+      count++
+    }
+  }
+
+  return count
+}
+
 export const calculateConsecutiveDays = (task: any) => {
   const { datesCompleted } = task
 
-  let count = 0
-
   if (datesCompleted.length === 0) {
-    return count
+    return 0
   }
 
   const sortedDates = task.datesCompleted
     .sort((a: any, b: any) => new Date(a).setHours(0, 0, 0, 0) - new Date(b).setHours(0, 0, 0, 0))
     .reverse()
 
-  const today = new Date()
-  const yesterday = addDays(today, -1)
-
-  if (today.toLocaleDateString() === sortedDates[0]) {
-    console.log('Catch 1')
-
-    if (yesterday.toLocaleDateString() === sortedDates[1]) {
-      for (let i = 0; i < sortedDates.length; i++) {
-        const date = new Date(sortedDates[i])
-        const comparisonDate = new Date(sortedDates[i + 1])
-
-        if (addDays(date, -1).toLocaleDateString() !== comparisonDate.toLocaleDateString()) {
-          count++
-          break
-        } else {
-          count++
-        }
-      }
-    }
-
-  } else if (yesterday.toLocaleDateString() === sortedDates[0]) {
-
-    for (let i = 0; i < sortedDates.length; i++) {
-      const date = new Date(sortedDates[i])
-      const comparisonDate = new Date(sortedDates[i + 1])
-
-      if (addDays(date, -1).toLocaleDateString() !== comparisonDate.toLocaleDateString()) {
-        count++
-        break
-      } else {
-        count++
-      }
-    }
-  }
+  const count = countConsecutiveCompletionDays(sortedDates)
 
   return count
 }
+
